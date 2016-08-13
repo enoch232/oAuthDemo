f65c2e16aeaf710784d6e02f48c97526ea0d93b7
enoch232 <enoch232@gmail.com>
2016-08-12 23:13:41 -0700

    add view page for new registration, and error message, and add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'users/new'
 
   get 'users/create'
-
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
