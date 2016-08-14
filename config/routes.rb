6bb08c3ab9f908c12cdb92cbbdadc8f77370e7ab
enoch232 <enoch232@gmail.com>
2016-08-13 20:53:43 -0700

    add pages controller and add logout path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  get 'pages/index'
 
   resources :users, only: [:new, :create]
+  delete '/logout', to: 'sessions#destroy', as: :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
